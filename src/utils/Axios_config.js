a8d8cb9b48adbc2c7a31d29d4ba742af70e1cd8c
Matei Dragutu <mateidr7@gmail.com>
2025-07-21 01:31:22 +0300

    sda

--- src/utils/Axios_config.js
+++ src/utils/Axios_config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const AXIOS = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.REACT_APP_BACKEND_LINK,
   headers: {
     "Content-Type": "application/json",
   },
